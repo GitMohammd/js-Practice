174e975e1feaf33f8d5c840c5c460a7992ce05f2
shamim <shamim@createdbycocoon.com>
2022-11-11 12:45:45 +0600

    js class

--- class.js
+++ class.js
@@ -1,1 +1,30 @@
 // asdf
+let rectengle = class {
+  constructor(height, width) {
+    this.height = height;
+    this.width = width;
+  }
+};
+// console.log(rectengle.name);
+
+rectengle = class rectengle2 {
+  constructor(height, width) {
+    this.height = height;
+    this.width = width;
+  }
+};
+// console.log(rectengle.name);
+
+class House {
+  constructor(color) {
+    this.color = color;
+  }
+  getFarniture() {
+    return "sofa";
+  }
+}
+
+const houseObject = new House("red");
+
+console.log(houseObject, houseObject.color);
+console.log(houseObject.getFarniture());
